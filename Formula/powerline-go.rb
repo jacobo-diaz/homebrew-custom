112794dff9443d75f1f0ee4e4c2870e7b2aa1950
Jacobo Díaz García <jacobo.diaz@udc.es>
2020-02-03 20:47:20 +0100

    Added zlua formula

--- Formula/powerline-go.rb
+++ Formula/powerline-go.rb
@@ -1,5 +1,5 @@
 class PowerlineGo < Formula
-  desc " A beautiful and useful low-latency prompt for your shell, written in go"
+  desc "A beautiful and useful low-latency prompt for your shell, written in go"
   homepage "https://github.com/justjanne/powerline-go"
   version "1.16.0"
   url "https://github.com/jacobo-diaz/powerline-go/releases/download/v#{version}/powerline-go"
---
+++ Formula/zlua.rb
@@ -0,0 +1,11 @@
+class Zlua < Formula
+  desc "A new cd command that helps you navigate faster by learning your habits"
+  homepage "https://github.com/skywind3000/z.lua"
+  version "1.7.4"
+  url "https://github.com/skywind3000/z.lua/archive/v#{version}.tar.gz"
+
+  def install
+    bin.install "z.lua" => "z.lua"
+  end
+
+end
