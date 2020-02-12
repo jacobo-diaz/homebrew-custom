5bff33cc55793537d79f1344301015dd6b391f67
Jacobo Díaz García <jacobo.diaz@udc.es>
2020-02-12 12:08:17 +0100

    Updated zlua

--- Formula/zlua.rb
+++ Formula/zlua.rb
@@ -2,7 +2,7 @@ class Zlua < Formula
   desc "A new cd command that helps you navigate faster by learning your habits"
   homepage "https://github.com/skywind3000/z.lua"
   version "1.8.4"
-  url "https://github.com/skywind3000/z.lua/archive/v#{version}.tar.gz"
+  url "https://github.com/skywind3000/z.lua/archive/#{version}.tar.gz"
 
   def install
     bin.install "z.lua" => "z.lua"
