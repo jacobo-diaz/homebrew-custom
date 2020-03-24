eb8d62c2478784a6ecd1124cbc0f40285046bb46
Jacobo Díaz García <jacobo.diaz@udc.es>
2020-03-24 20:14:24 +0100

    powerline go

--- Formula/powerline-go.rb
+++ Formula/powerline-go.rb
@@ -1,7 +1,7 @@
 class PowerlineGo < Formula
   desc "A beautiful and useful low-latency prompt for your shell, written in go"
   homepage "https://github.com/justjanne/powerline-go"
-  version "1.16.0"
+  version "1.17.0"
   url "https://github.com/jacobo-diaz/powerline-go/releases/download/v#{version}/powerline-go"
   sha256 "f8ff2e6294ef5926a2c0e4c21fb611df78a447e1397bfe1d65c7d3e8bbe1b7ad"
 
