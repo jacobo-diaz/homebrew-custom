49ffd89195e57451388405ae66a7213284c16e2f
Jacobo Díaz García <jacobo.diaz@udc.es>
2020-06-30 10:58:19 +0200

    Updated zlua and removed sha in powerline

--- Formula/zlua.rb
+++ Formula/zlua.rb
@@ -1,7 +1,7 @@
 class Zlua < Formula
   desc "A new cd command that helps you navigate faster by learning your habits"
   homepage "https://github.com/skywind3000/z.lua"
-  version "1.8.4"
+  version "1.8.7"
   url "https://github.com/skywind3000/z.lua/archive/#{version}.tar.gz"
 
   def install
